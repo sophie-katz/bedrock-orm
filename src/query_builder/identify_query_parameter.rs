9279d527dca861b85ebe858b6af70d696a783be0
Sophie Katz <sophie@sophiekatz.us>
2023-07-11 13:02:05 +0000

    Implemeneted for integers

--- src/query_builder/identify_query_parameter.rs
+++ src/query_builder/identify_query_parameter.rs
@@ -15,4 +15,24 @@
 
 use std::{fmt::Debug, hash::Hash};
 
+/// A type that can be used to identify a query parameter for both the query itself and any value
+/// that can be bound to the query.
+///
+/// - If your query passes in parameters by index, you can use `usize` as your query parameter
+///   type.
 pub trait IdentifyQueryParameter: Debug + Hash + PartialEq {}
+
+impl IdentifyQueryParameter for usize {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn uses_query_parameter<T: IdentifyQueryParameter>(_: T) {}
+
+    #[test]
+    fn integer_indexed() {
+        uses_query_parameter(0usize);
+        uses_query_parameter(1usize);
+    }
+}
